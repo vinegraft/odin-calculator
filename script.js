58feb13cf1092192e5cd46472ad0c0e342cbe0ba
vinegraft <982042+vinegraft@users.noreply.github.com>
2024-09-19 17:44:35 -0400

    Add operate() function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let operand1 = null;
+let operand2 = null;
+let operator = null;
+
 function add(a, b) {
   return a + b;
 }
@@ -15,4 +19,18 @@ function divide(a, b) {
     return null;
   }
   return a / b;
+}
+
+function operate(op, a, b) {
+  if (op === "add") {
+    return add(a, b);
+  } else if (op === "subtract") {
+    return subtract(a, b);
+  } else if (op === "multiply") {
+    return multiply(a, b);
+  } else if (op === "divide") {
+    return divide(a, b);
+  } else {
+    return null;
+  }
 }
