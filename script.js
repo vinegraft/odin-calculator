25e6116f02290be5eb46608b5e9a73c7ad40710a
vinegraft <982042+vinegraft@users.noreply.github.com>
2024-09-20 18:35:32 -0400

    Add preliminary display updates from pressed numbers

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+// Three slashes (///) indicates pseudocode
+
+const MAX_DIGITS = 10;
+let hasDecimal = false;
+let displayValue = 0;
 let operand1 = null;
 let operand2 = null;
 let operator = null;
+
+const display = document.querySelector(".display");
+const numberButtons = document.querySelectorAll(".number");
+numberButtons.forEach((numberButton) => {
+  numberButton.addEventListener("click", (event) => {
+    let buttonClasses = Array.from(event.currentTarget.classList);
+    let numberWord = buttonClasses.filter(
+      (buttonClass) => buttonClass != "number" && buttonClass != "button"
+    );
+    numberWord = convertWordToInteger(numberWord.toString());
+    populateDisplay(numberWord);
+  });
+});
 
 function add(a, b) {
   return a + b;
@@ -32,5 +50,46 @@ function operate(op, a, b) {
     return divide(a, b);
   } else {
     return null;
+  }
+}
+
+function convertWordToInteger(word) {
+  switch (word) {
+    case "one":
+      return 1;
+    case "two":
+      return 2;
+    case "three":
+      return 3;
+    case "four":
+      return 4;
+    case "five":
+      return 5;
+    case "six":
+      return 6;
+    case "seven":
+      return 7;
+    case "eight":
+      return 8;
+    case "nine":
+      return 9;
+    case "zero":
+      return 0;
+    default:
+      return null;
+  }
+}
+
+function populateDisplay(button) {
+  if (typeof button === "number") {
+    displayValue = displayValue.toString();
+    displayValue += button.toString();
+    display.textContent = displayValue;
+    displayValue = parseFloat(displayValue);
+    //TODO handle leading zeroes and max display digits
+  }
+  if (button === "decimal" && !hasDecimal) {
+    //TODO handle trailing decimal and zeroes (1. or 1.0 wants? to convert to 1)
+    hasDecimal = true;
   }
 }
